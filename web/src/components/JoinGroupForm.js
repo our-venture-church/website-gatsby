09de10009b3179408ff5cb22258afecb68d60252
Jim Arment <jima@zillowgroup.com>
2019-11-02 17:23:20 -0700

    Better form submission experience.

--- web/src/components/JoinGroupForm.js
+++ web/src/components/JoinGroupForm.js
@@ -2,11 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import colors from '../theme/tokens/colors';
 
-const StyledForm = styled.form`
+const StyledFormWrapper = styled.div`
     margin: auto;
     max-width: 700px;
     width: 100%;
 `;
+
+const StyledForm = styled.form``;
 
 const StyledFormField = styled.div`
     margin-bottom: 2rem;
@@ -58,19 +60,24 @@ class JoinGroupForm extends React.Component {
             groupName,
             groupNumber,
             groupPage,
+            sendSuccess: false,
+            sendError: false,
         };
     }
 
     /* Here’s the juicy bit for posting the form submission */
 
     handleSubmit = e => {
+        this.setState({ sendError: false, errorMessage: null });
         fetch('/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: encode({ 'form-name': 'join-a-group', ...this.state }),
         })
-            .then(() => alert('Success!'))
-            .catch(error => alert(error));
+            .then(this.setState({ sendSuccess: true }))
+            .catch(error =>
+                this.setState({ sendError: true, errorMessage: error })
+            );
 
         e.preventDefault();
     };
@@ -78,49 +85,83 @@ class JoinGroupForm extends React.Component {
     handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
     render() {
-        const { name, email, groupName, groupNumber, groupPage } = this.state;
+        const {
+            name,
+            email,
+            groupName,
+            groupNumber,
+            groupPage,
+            sendSuccess,
+            sendError,
+        } = this.state;
+        const showMessage = sendSuccess || sendError;
+        const message =
+            showMessage && sendSuccess
+                ? "Your request was sent. We'll contact you with more information."
+                : 'There was a problem sending your request. Please email your request to groups@ourventure.church.';
         return (
-            <StyledForm
-                action="POST"
-                name="join-a-group"
-                onSubmit={this.handleSubmit}
-                data-netlify="true"
-            >
+            <StyledFormWrapper>
                 <h3>Sign-up</h3>
-                <p>
-                    Fill out this form and we'll contact you about joining this
-                    group.
-                </p>
-                <input type="hidden" name="groupName" value={groupName} />
-                <input type="hidden" name="groupNumber" value={groupNumber} />
-                <input type="hidden" name="groupPage" value={groupPage} />
-                <StyledFormField>
-                    <label htmlFor="join-form-name">Name</label>
-                    <input
-                        id="join-form-name"
-                        type="text"
-                        name="name"
-                        value={name}
-                        onChange={this.handleChange}
-                        required
-                    />
-                </StyledFormField>
-                <StyledFormField>
-                    <label htmlFor="join-form-email">Email</label>
-                    <input
-                        id="join-form-email"
-                        type="email"
-                        name="email"
-                        value={email}
-                        onChange={this.handleChange}
-                        required
-                    />
-                </StyledFormField>
-                <div data-netlify-recaptcha="true"></div>
-                <StyledFormField>
-                    <button type="submit">Send Join Request</button>
-                </StyledFormField>
-            </StyledForm>
+
+                {showMessage ? (
+                    <p>{message}</p>
+                ) : (
+                    <React.Fragment>
+                        <p>
+                            Fill out this form and we'll contact you about
+                            joining this group.
+                        </p>
+                        <StyledForm
+                            action="POST"
+                            name="join-a-group"
+                            onSubmit={this.handleSubmit}
+                            data-netlify="true"
+                        >
+                            <input
+                                type="hidden"
+                                name="groupName"
+                                value={groupName}
+                            />
+                            <input
+                                type="hidden"
+                                name="groupNumber"
+                                value={groupNumber}
+                            />
+                            <input
+                                type="hidden"
+                                name="groupPage"
+                                value={groupPage}
+                            />
+                            <StyledFormField>
+                                <label htmlFor="join-form-name">Name</label>
+                                <input
+                                    id="join-form-name"
+                                    type="text"
+                                    name="name"
+                                    value={name}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                            </StyledFormField>
+                            <StyledFormField>
+                                <label htmlFor="join-form-email">Email</label>
+                                <input
+                                    id="join-form-email"
+                                    type="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                            </StyledFormField>
+                            <div data-netlify-recaptcha="true"></div>
+                            <StyledFormField>
+                                <button type="submit">Send Join Request</button>
+                            </StyledFormField>
+                        </StyledForm>
+                    </React.Fragment>
+                )}
+            </StyledFormWrapper>
         );
     }
 }
