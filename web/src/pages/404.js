53dc644111be46236881155067db2ff8692106f0
Jim Arment <jima@zillowgroup.com>
2019-11-12 11:48:25 -0800

    Custom 404 page

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,14 +1,26 @@
-import React from "react"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import NarrowPageWrapper from '../layouts/NarrowPageWrapper';
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+    <Layout>
+        <SEO title="404: Not found" />
+        <NarrowPageWrapper includeSidePadding={true} includeTopPadding={true}>
+            <h1>Page not found</h1>
+            <p>
+                We just recently launched this version of our site, and some
+                pages are still missing. Sadly, you found one. But hang tight we
+                hope to have everything ready to go in a few weeks.
+            </p>
+            <p>
+                Use the main navigation to find other pages on the site, or{' '}
+                <Link to="/contact">contact us</Link> if you need more help.
+            </p>
+        </NarrowPageWrapper>
+    </Layout>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
