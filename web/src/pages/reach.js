5ac162bd4c7b1a5b2551915886c7464b1f768dac
Jim Arment <jima@zillowgroup.com>
2019-12-17 16:06:14 -0800

    comment-out unused imports

--- web/src/pages/reach.js
+++ web/src/pages/reach.js
@@ -5,8 +5,8 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import BlockContent from '../components/block-content';
 import styled from 'styled-components';
-import { buildImageObj } from '../lib/helpers';
-import { imageUrlFor } from '../lib/image-url';
+// import { buildImageObj } from '../lib/helpers';
+// import { imageUrlFor } from '../lib/image-url';
 import { getDefaultPadding } from '../utils/styles';
 import colors from '../theme/tokens/colors';
 
