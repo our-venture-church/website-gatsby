5f61d70b45303d6081ab396cd8ec6d3861d82136
Jim Arment <jima@zillowgroup.com>
2020-03-26 08:18:55 -0700

    Add live stream embed to the live stream page

--- web/src/pages/live.js
+++ web/src/pages/live.js
@@ -13,6 +13,7 @@ import Facebook from '../components/icons/facebook';
 
 const LiveStreamTitle = styled.h1`
     font-size: 24px;
+    margin-bottom: 2rem;
     text-align: center;
 `;
 
@@ -48,6 +49,21 @@ const StreamingLink = styled.a`
     }
 `;
 
+const EmbedContainer = styled.div`
+    padding-top: 56.25%;
+    position: relative;
+
+    iframe,
+    object,
+    embed {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+`;
+
 const LiveStreamPage = props => {
     const data = useStaticQuery(graphql`
         query LiveStreamPageQuery {
@@ -71,6 +87,14 @@ const LiveStreamPage = props => {
     } = data.sanityLiveStreamPage;
     const { title, seoDescription } = pageIntro;
 
+    // const iframeSrc =
+    //     'https://yourstreamlive.com/embed/iframe/live/6655?&autostart=no&image_url=https://cdn.sanity.io/images/3deuluc8/prod/1f39db72bb0d5a9001a5a7385d182b22a35a93ef-5760x3840.jpg?rect=0,300,5760,3240&w=1200&h=675&fit=crop&auto=format&responsive=yes&aspect_ratio=16:9';
+    const iframeAttrs = {
+        allow: 'autoplay',
+        src:
+            'https://yourstreamlive.com/embed/iframe/live/6655?&responsive=yes&aspect_ratio=16:9',
+    };
+
     return (
         <Layout>
             <SEO title={title} description={seoDescription} />
@@ -78,21 +102,20 @@ const LiveStreamPage = props => {
             <Banner>
                 <NarrowPageWrapper includePadding>
                     <LiveStreamTitle>{title}</LiveStreamTitle>
-                    <StreamingList>
-                        <StreamingListItem>
-                            <StreamingLink href={facebookStreamingLink}>
-                                <Facebook aria-hidden />
-                                <span>Facebook</span>
-                            </StreamingLink>
-                        </StreamingListItem>
-                        <StreamingListItem>
-                            <StreamingLink href={youtubeStreamingLink}>
-                                <YouTube aria-hidden />
-                                <span>YouTube</span>
-                            </StreamingLink>
-                        </StreamingListItem>
-                    </StreamingList>
                 </NarrowPageWrapper>
+                <EmbedContainer>
+                    <iframe
+                        frameborder="0"
+                        marginheight="0"
+                        marginwidth="0"
+                        scrolling="auto"
+                        allowfullscreen="yes"
+                        allowtransparency="yes"
+                        name="live"
+                        id="live"
+                        {...iframeAttrs}
+                    ></iframe>
+                </EmbedContainer>
             </Banner>
 
             <NarrowPageWrapper includePadding>
